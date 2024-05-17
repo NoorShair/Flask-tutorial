887fbe9a3e5aa2e2ddc0758cfe17a284439dbacd
noorshair <noor@coopersmith.org>
2024-05-17 16:00:28 +0300

    templates fix

--- app/__init__.py
+++ app/__init__.py
@@ -33,7 +33,6 @@ def create_app(config_class=Config):
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
 
-
     if not app.debug and not app.testing:
         if app.config['MAIL_SERVER']:
             auth = None
@@ -46,14 +45,14 @@ def create_app(config_class=Config):
             mail_handler = SMTPHandler(
                 mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
                 fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-                toaddrs=app.config['ADMINS'], subject='demo Failure',
+                toaddrs=app.config['ADMINS'], subject='Microblog Failure',
                 credentials=auth, secure=secure)
             mail_handler.setLevel(logging.ERROR)
             app.logger.addHandler(mail_handler)
 
         if not os.path.exists('logs'):
             os.mkdir('logs')
-        file_handler = RotatingFileHandler('logs/demo.log',
+        file_handler = RotatingFileHandler('logs/microblog.log',
                                            maxBytes=10240, backupCount=10)
         file_handler.setFormatter(logging.Formatter(
             '%(asctime)s %(levelname)s: %(message)s '
@@ -62,7 +61,7 @@ def create_app(config_class=Config):
         app.logger.addHandler(file_handler)
 
         app.logger.setLevel(logging.INFO)
-        app.logger.info('Demo startup')
+        app.logger.info('emo startup')
 
     return app
 
